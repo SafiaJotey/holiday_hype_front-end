3c65de2d222b8078fb388a9a5541633efbd9b952
SafiaJotey <safiaahmed1786@gmail.com>
2022-11-03 17:32:04 +0600

    fix header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,35 +3,43 @@ import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import logo from '../../Images/logo.png';
 import useAuth from '../Hooks/useAuth';
-import './Header.css';
 
 const Header = () => {
   const { user, logout } = useAuth();
 
   return (
     <>
-      <Navbar className="menu" collapseOnSelect expand="lg" variant="dark">
+      <Navbar
+        className="w-100 bg-primary"
+        collapseOnSelect
+        expand="lg"
+        variant="dark"
+      >
         <Container>
           <Navbar.Brand as={Link} to="/home" className="logo">
-            <img src={logo} alt="logo" />
+            <img className="w-50" src={logo} alt="logo" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="ms-auto d-flex  align-items-center">
-              <Nav.Link as={Link} to="/home">
+            <Nav className="ms-auto d-flex  align-items-center ">
+              <Nav.Link className="text-white" as={Link} to="/home">
                 Home
               </Nav.Link>
-              <Nav.Link as={Link} to="/addBlog">
+              <Nav.Link as={Link} className="text-white" to="/addBlog">
                 {' '}
                 Blogs
               </Nav.Link>
 
               {user?.email ? (
                 <div className="d-md-flex  align-items-center">
-                  <Nav.Link as={Link} to="/myOrder">
+                  <Nav.Link as={Link} className="text-white" to="/myOrder">
                     MyOrders
                   </Nav.Link>
-                  <Nav.Link as={Link} to="/myBlog/:email">
+                  <Nav.Link
+                    as={Link}
+                    className="text-white"
+                    to="/myBlog/:email"
+                  >
                     MyBlogs
                   </Nav.Link>
 
@@ -43,14 +51,14 @@ const Header = () => {
                         : `${user?.email}`}
                     </a>
                   </Navbar.Text>
-                  <Nav.Link as={Link} to="/home">
+                  <Nav.Link as={Link} className="text-white" to="/home">
                     <Button onClick={logout} variant="light">
                       Logout
                     </Button>{' '}
                   </Nav.Link>
                 </div>
               ) : (
-                <Nav.Link as={Link} to="/login">
+                <Nav.Link as={Link} className="text-white" to="/login">
                   Login
                 </Nav.Link>
               )}
